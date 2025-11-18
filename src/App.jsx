f25febac29b2b5b4a173bf4be49e283bed9692f6
Alan Ambrose <alanambrosedev@gmail.com>
2025-11-18 20:38:04 +0530

    feat: implement Blog component and update App to display blog posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import Another from "./components/Another";
 import Card from "./components/Card";
+import Blog from "./components/Blog";
+
 
 export default function App() {
-
-  return  (
-    <div>
-      <Card title="my title 1" content = "content 1" />
-      <Card title="my title 2" content = "content 2" />
-      <Another />
-    </div>
-  )
+  //  <div>
+  //     <Card title="my title 1" content = "content 1" />
+  //     <Card title="my title 2" content = "content 2" />
+  //     <Another />
+  //   </div>
+  // const increment = () => {
+  //   setCount(count+1);
+  //   console.log("incremented", count);
+    
+  // };
+  // const [count, setCount] = useState(2);
+  // return  (
+  //   <div>
+  //    <h1>{count}</h1>
+  //    <button onClick={increment}>+</button>
+  //   </div>
+  // )
+  const blogs = [
+    { title: "Title 1", content: "Content 1"},
+    { title: "Title 2", content: "Content 2"},
+    { title: "Title 3", content: "Content 3"},
+  ];
+  return <div>
+    <h1>Blogs</h1>
+    <hr />
+    {blogs.map(blog => <Blog title={blog.title} content={blog.content} />) }
+  </div>
 }
+ 
---
+++ src/components/Blog.jsx
@@ -0,0 +1,6 @@
+export default function Blog({title, content}) {
+  return <div>
+    <h1>{title}</h1>
+    <p>{content}</p>
+  </div>
+}
